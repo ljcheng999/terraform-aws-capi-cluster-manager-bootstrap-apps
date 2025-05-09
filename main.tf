b0f42dca7e9eb17a71d04ac4eb9b5f2d9eba13ae
jcheng <johnny.cheng@charter.com>
2025-05-09 00:42:34 -0600

    test a new module and cleanup

--- main.tf
+++ main.tf
@@ -9,9 +9,8 @@ provider "aws" {
 
 
 module "capi_cluster_manager_bootstrap_app" {
-  # source                                           = "../../modules/terraform-aws-capi-cluster-manager-bootstrap-apps"
-  source  = "ljcheng999/capi-cluster-manager-bootstrap-apps/aws"
-  version = "1.0.0-alpha"
+  source                                           = "ljcheng999/capi-cluster-manager-bootstrap-apps/aws"
+  version                                          = "1.0.0-alpha"
 
 
   create                                           = local.create
