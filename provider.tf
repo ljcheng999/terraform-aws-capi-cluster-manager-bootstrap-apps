1f653a6cc180e0b5cd8b80f058df0c96011a764c
jcheng <johnny.cheng@charter.com>
2025-05-09 00:16:08 -0600

    added tolerations and affinity

--- provider.tf
+++ provider.tf
@@ -21,9 +21,9 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                    = data.aws_eks_cluster.this[0].endpoint
-  cluster_ca_certificate  = base64decode(data.aws_eks_cluster.this[0].certificate_authority[0].data)
-  token                   = data.aws_eks_cluster_auth.this[0].token
+  host                     = data.aws_eks_cluster.this[0].endpoint
+  cluster_ca_certificate   = base64decode(data.aws_eks_cluster.this[0].certificate_authority[0].data)
+  token                    = data.aws_eks_cluster_auth.this[0].token
 }
 
 provider "helm" {  
