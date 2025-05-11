c8b5166646a86dafc36456a91820ef56abdfb189
jcheng <johnny.cheng@charter.com>
2025-05-10 19:28:20 -0600

    test new version

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ provider "aws" {
 
 
 module "capi_cluster_manager_bootstrap_app" {
-  # source                                           = "../../modules/terraform-aws-capi-cluster-manager-bootstrap-apps"
   source                                           = "ljcheng999/capi-cluster-manager-bootstrap-apps/aws"
   version                                          = "1.0.0-beta"
 
@@ -22,7 +21,6 @@ module "capi_cluster_manager_bootstrap_app" {
   create_aws_elb_controller                        = local.create_aws_elb_controller
   helm_release_aws_elb_controller_parameter        = local.helm_release_aws_elb_controller_parameter
   helm_release_aws_elb_controller_sets_parameter   = local.helm_release_aws_elb_controller_sets_parameter
-
 
   ### ArgoCD
   create_argocd                                    = local.create_argocd
