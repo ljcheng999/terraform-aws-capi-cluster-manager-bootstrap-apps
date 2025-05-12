c0c2477ebd946bc8049611a0b1fedff2068e5dd0
jcheng <johnny.cheng@charter.com>
2025-05-11 20:57:24 -0600

    test beta13

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 module "capi_cluster_manager_bootstrap_app" {
   # source  = "../../modules/terraform-aws-capi-cluster-manager-bootstrap-apps"
   source  = "ljcheng999/capi-cluster-manager-bootstrap-apps/aws"
-  version = "1.0.0-beta11"
+  version = "1.0.0-beta13"
 
   create                                           = local.create
   cluster_name                                     = local.cluster_name
